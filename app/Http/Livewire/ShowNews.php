a71a111fafd91641fe4e26f187f433ca421584e3
abrorbeksoft <abrorbek.ubaydullayev98@gmail.com>
2020-11-24 12:33:01 +0500

    Initial commit

---
+++ app/Http/Livewire/ShowNews.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ShowNews extends Component
+{
+
+    public $name="Abror";
+
+    public function render()
+    {
+        return view('livewire.show-news');
+    }
+}
---
+++ resources/views/livewire/navbar.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row badge-dark text-white">
+        <div class="col-md-3">
+            <a class="navbar-logo" href="#">Logo</a>
+        </div>
+        <div class="col-md-9 ">
+            <div class="row justify-content-end">
+                <div class="list-item"><a class="list-link" href="#">Link 1</a></div>
+                <div class="list-item"><a class="list-link" href="#">Link 2</a></div>
+                <div class="list-item"><a class="list-link" href="#">Link 3</a></div>
+                <div class="list-item"><a class="list-link" href="#">Link 4</a></div>
+            </div>
+        </div>
+    </div>
+</div>
